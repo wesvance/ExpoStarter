31e87aae9025d3f788233b06ae62fac52999988a
Wesley Vance <me@wesvance.com>
2022-11-24 22:27:18 -0500

    Updated storybooks webpack to fix deployment and updated the gh action

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,5 +5,11 @@ const { withUnimodules } = require('@expo/webpack-config/addons')
 module.exports = ({ config }) => {
   config.resolve.modules = [path.resolve(__dirname, '..', 'src'), 'node_modules']
   config.resolve.alias = { 'react-native': 'react-native-web' }
-  return withUnimodules(config)
+  const wUni = withUnimodules(config, {
+    projectRoot: path.resolve(__dirname, '../')
+  })
+
+  // Needed when using expo: https://github.com/storybookjs/storybook/issues/11694#issuecomment-667466157
+  wUni.output.publicPath = ''
+  return wUni
 }
